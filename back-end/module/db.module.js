6cc31c10c08b9c0735cd080ac28cc84fc0394b51
aditya maurya <aditya2018m@gmail.com>
2023-11-29 20:35:38 +0530

    back-end

--- back-end/module/db.module.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-// let url = "mongodb://127.0.0.1:27017/mangers";
-const url =
-  "mongodb+srv://adityamaurya07:42nwFsAf53aCUEkS@cluster0.8wr3ax0.mongodb.net/";
-
-mongoose.connect(url);
-console.log("connected");
--- back-end/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Get, Post } from "../controller/user.controller.js";
-
-const router = express.Router();
-
-router.get("/", Get);
-router.post("/", Post);
-
-export default router;
